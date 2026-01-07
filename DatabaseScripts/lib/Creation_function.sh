b061c75a7ffa69c50553a89924d040aedd078008
mohesham59 <mohesham357@gmail.com>
2026-01-07 17:27:55 +0200

    Fix database creation function and bash formatting

--- DatabaseScripts/lib/Creation_function.sh
+++ DatabaseScripts/lib/Creation_function.sh
@@ -9,24 +9,22 @@ CreationFunction() {
   local Db_Name
   read -p "Enter Data Base Name please: " Db_Name
  
- # using validation function to validated the entered name #
+  # using validation function to validated the entered name #
  
   if ! validate_db_name "$Db_Name" ; then
     echo "Please try again with a valid database name."
-     return 1
+    return 1
   fi
 
-# validate if the data base alrady exect or no #
-
-  if [[ -e "$PROJECT_DIR/$Db_Name" ]]; then
-   echo "The database '$Db_Name' already exists in $PROJECT_DIR."
-   return 1
-
+  # FIXED: validate if the database already exists or not
+  local db_path="$PROJECT_ROOT/storage/databases/$Db_Name"
+  
+  if [[ -e "$db_path" ]]; then
+    echo "The database '$Db_Name' already exists in $db_path."
+    return 1
   else
-	local db_path="$PROJECT_ROOT/storage/databases/$Db_Name"
-	mkdir -p "$db_path/tables"
-	mkdir -p "$db_path/metadata"
-
-    echo "The database '$Db_Name' was created :) in $db_path"
+    mkdir -p "$db_path/tables"
+    mkdir -p "$db_path/metadata"
+    echo "The database '$Db_Name' was created successfully in $db_path"
   fi
 }
